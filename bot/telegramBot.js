cfa856b46e102bee58c3445c255551371ad168c9
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2025-04-22 20:03:20 +0530

    Update telegramBot.js

--- bot/telegramBot.js
+++ bot/telegramBot.js
@@ -1,9 +1,20 @@
+// bot/telegramBot.js
 const { Telegraf } = require('telegraf');
 require('dotenv').config();
 const mongoose = require('mongoose');
 const { parseMovieMessage, saveParsedMovie } = require('./services/movieParser');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('MongoDB connection error:', err);
+});
 
 bot.start((ctx) => ctx.reply('Welcome! I am your Movie Telegram Sync Bot.'));
 
