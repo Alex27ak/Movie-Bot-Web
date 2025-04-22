594ed78b6751a71cdfad84c3cf1948b724e76a9e
Alex27ak <96329236+Alex27ak@users.noreply.github.com>
2025-04-22 20:31:48 +0530

    Update movies.js

--- routes/movies.js
+++ routes/movies.js
@@ -4,14 +4,24 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie');
 
+// Fetch all movies with basic fields (title, year, language)
 router.get('/', async (req, res) => {
   const movies = await Movie.find({}, 'title year language').sort({ updatedAt: -1 });
   res.json(movies);
 });
 
+// Fetch movie by ID to show detailed information
 router.get('/:id', async (req, res) => {
   const movie = await Movie.findById(req.params.id);
   res.json(movie);
+});
+
+// Search for movies by title (case-insensitive)
+router.get('/search', async (req, res) => {
+  const query = req.query.query;
+  const regex = new RegExp(query, 'i');
+  const movies = await Movie.find({ title: { $regex: regex } }, 'title year language').sort({ updatedAt: -1 });
+  res.json(movies);
 });
 
 module.exports = router;
